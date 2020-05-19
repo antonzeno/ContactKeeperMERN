03f97dca0fff845bb0e8e5715b3cbc5b9a10f913
Toni <tonizeno5@gmail.com>
2020-05-19 16:43:11 +0200

    Prepare for deploy

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className="my-1">
+        Full Stack React App for keeping user contacts.
+        <br />
+        &copy; Andon Zeno
+      </p>
+      <p className="bg-dark p">
+        <strong>Version 1.0.0</strong>
+      </p>
+    </div>
+  )
+}
+
+export default About
